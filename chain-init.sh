21b35a69fb7807cff9895664799a800f5ce87f79
Sean Batzel <romulus108@protonmail.com>
2024-05-10 17:39:51 -0400

    Fixing finalization.

--- chain-init.sh
+++ chain-init.sh
@@ -1,3 +1,3 @@
 /app/fennel-node key insert --key-type aura --base-path /app/chain --chain /app/fennelSpecRaw.json --scheme Sr25519 --suri "$chainsuri"
-/app/fennel-node key insert --key-type gran --base-path /app/chain --chain customSpecRaw.json --scheme Ed25519 --suri "$chainsuri"
+/app/fennel-node key insert --key-type gran --base-path /app/chain --chain /app/fennelSpecRaw.json --scheme Ed25519 --suri "$chainsuri"
 /app/fennel-node --base-path /app/chain --chain /app/fennelSpecRaw.json --port 30333 --rpc-port 9945 --node-key 0000000000000000000000000000000000000000000000000000000000000001 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --rpc-cors all --prometheus-external --rpc-methods Unsafe --rpc-external
