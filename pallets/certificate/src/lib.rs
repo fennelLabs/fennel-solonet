3703e53da30f62fbd719b9fd7931e8ca1a1cbab0
neurosx <neurosx@localhost>
2025-05-13 18:06:19 -0400

    Update certificate pallet lib.rs to 2503

--- pallets/certificate/src/lib.rs
+++ pallets/certificate/src/lib.rs
@@ -19,7 +19,7 @@ const CERTIFICATE_EXISTS: bool = true;
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::{
-		dispatch::DispatchResult,
+        dispatch::{DispatchResultWithPostInfo, DispatchResult},
 		pallet_prelude::*,
 		traits::{Currency, LockIdentifier, LockableCurrency, WithdrawReasons},
 	};
@@ -32,17 +32,13 @@ pub mod pallet {
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		/// The overarching event type.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-		/// Weight information for extrinsics in this pallet.
 		type WeightInfo: WeightInfo;
 		type Currency: LockableCurrency<
 			Self::AccountId,
 			Moment = frame_system::pallet_prelude::BlockNumberFor<Self>,
 		>;
-		/// The identifier for the lock used to store certificate deposits.
 		type LockId: Get<LockIdentifier>;
-		/// The price of a certificate lock.
 		type LockPrice: Get<u32>;
 	}
 
@@ -51,7 +47,6 @@ pub mod pallet {
 
 	#[pallet::storage]
 	#[pallet::getter(fn certificate_list)]
-	/// Maps accounts to the array of identities it owns.
 	pub type CertificateList<T: Config> = StorageDoubleMap<
 		_,
 		Blake2_128Concat,
@@ -65,49 +60,34 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// A `certificate` was sent.
-		CertificateSent(T::AccountId, T::AccountId),
-		/// A `certificate` was revoked.
-		CertificateRevoked(T::AccountId, T::AccountId),
-		CertificateLock(<T as frame_system::Config>::AccountId, BalanceOf<T>),
-		CertificateUnlock(<T as frame_system::Config>::AccountId, BalanceOf<T>),
+        CertificateSent { sender: T::AccountId, recipient: T::AccountId },
+        CertificateRevoked { sender: T::AccountId, recipient: T::AccountId },
+        CertificateLock { account: T::AccountId, amount: BalanceOf<T> },
+        CertificateUnlock { account: T::AccountId, amount: BalanceOf<T> },
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
-		/// The current account does not own the certificate.
 		CertificateNotOwned,
-		/// The certificate already exists.
 		CertificateExists,
 		InsufficientBalance,
 	}
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// Creates an on-chain event with a Certificate payload defined as part of the transaction
-		/// and commits the details to storage.
 		#[pallet::weight(T::WeightInfo::send_certificate())]
 		#[pallet::call_index(0)]
-		pub fn send_certificate(origin: OriginFor<T>, recipient: T::AccountId) -> DispatchResult {
+        pub fn send_certificate(origin: OriginFor<T>, recipient: T::AccountId) -> DispatchResultWithPostInfo {
 			let who = ensure_signed(origin)?;
-
 			if T::Currency::total_balance(&who) < T::Currency::minimum_balance() {
 				return Err(Error::<T>::InsufficientBalance.into());
 			}
-
 			ensure!(
 				!CertificateList::<T>::contains_key(&who, &recipient),
 				Error::<T>::CertificateExists
 			);
-			// Insert a placeholder value into storage - if the pair (who, recipient) exists, we
-			// know there's a certificate present for the pair, regardless of value.
 			T::Currency::set_lock(T::LockId::get(), &who, 10u32.into(), WithdrawReasons::all());
-
-			Self::deposit_event(Event::CertificateLock(
-				who.clone(),
-				T::Currency::free_balance(&who),
-			));
-
+            Self::deposit_event(Event::CertificateLock { account: who.clone(), amount: T::Currency::free_balance(&who) });
 			<CertificateList<T>>::try_mutate(
 				&who,
 				recipient.clone(),
@@ -116,34 +96,22 @@ pub mod pallet {
 					Ok(())
 				},
 			)?;
-
-			Self::deposit_event(Event::CertificateSent(recipient, who));
-
-			Ok(())
+            Self::deposit_event(Event::CertificateSent { sender: who.clone(), recipient: recipient.clone() });
+            Ok(().into())
 		}
-
 		#[pallet::weight(T::WeightInfo::revoke_certificate())]
 		#[pallet::call_index(1)]
-		/// Revokes the identity with ID number identity_id, as long as the identity is owned by
-		/// origin.
-		pub fn revoke_certificate(origin: OriginFor<T>, recipient: T::AccountId) -> DispatchResult {
+        pub fn revoke_certificate(origin: OriginFor<T>, recipient: T::AccountId) -> DispatchResultWithPostInfo {
 			let who = ensure_signed(origin)?;
-
 			if T::Currency::total_balance(&who) < T::Currency::minimum_balance() {
 				return Err(Error::<T>::InsufficientBalance.into());
 			}
-
 			ensure!(
 				CertificateList::<T>::contains_key(&who, &recipient),
 				Error::<T>::CertificateNotOwned
 			);
-
 			T::Currency::remove_lock(T::LockId::get(), &who);
-			Self::deposit_event(Event::CertificateUnlock(
-				who.clone(),
-				T::Currency::free_balance(&who),
-			));
-
+            Self::deposit_event(Event::CertificateUnlock { account: who.clone(), amount: T::Currency::free_balance(&who) });
 			<CertificateList<T>>::try_mutate(
 				&who,
 				recipient.clone(),
@@ -152,10 +120,8 @@ pub mod pallet {
 					Ok(())
 				},
 			)?;
-
-			Self::deposit_event(Event::CertificateRevoked(recipient, who));
-
-			Ok(())
+            Self::deposit_event(Event::CertificateRevoked { sender: who.clone(), recipient: recipient.clone() });
+            Ok(().into())
 		}
 	}
 }
