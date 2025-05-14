00e2fac8150c31d5ef25d04cc04a8a9c6b001c36
CorruptedAesthetic <53838755+CorruptedAesthetic@users.noreply.github.com>
2025-05-13 21:29:26 -0400

    signal mock.rs updated to 2503

--- pallets/signal/src/mock.rs
+++ pallets/signal/src/mock.rs
@@ -1,67 +1,40 @@
 use crate as pallet_signal;
-use frame_support::{derive_impl, parameter_types};
-use frame_system as system;
-use sp_core::{ConstU32, H256};
-use sp_runtime::{
-	traits::{BlakeTwo256, IdentityLookup},
-	BuildStorage,
-};
+use frame_support::derive_impl;
+use sp_core::{ConstU32, ConstU128};
+use sp_runtime::BuildStorage;
 
 pub type Balance = u128;
 
 type Block = frame_system::mocking::MockBlock<Test>;
 
-// Configure a mock runtime to test the pallet.
-frame_support::construct_runtime!(
-	pub enum Test
-	{
-		System: frame_system::{Pallet, Call, Config<T>, Storage, Event<T>},
-		SignalModule: pallet_signal::{Pallet, Call, Storage, Event<T>},
-		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
-	}
-);
+#[frame_support::runtime]
+mod runtime {
+    #[runtime::runtime]
+    #[runtime::derive(
+        RuntimeCall,
+        RuntimeEvent,
+        RuntimeError,
+        RuntimeOrigin,
+        RuntimeFreezeReason,
+        RuntimeHoldReason,
+        RuntimeSlashReason,
+        RuntimeLockId,
+        RuntimeTask
+    )]
+    pub struct Test;
 
-parameter_types! {
-	pub const BlockHashCount: u64 = 250;
-	pub const SS58Prefix: u8 = 42;
+    #[runtime::pallet_index(0)]
+    pub type System = frame_system::Pallet<Test>;
+    #[runtime::pallet_index(1)]
+    pub type Signal = pallet_signal::Pallet<Test>;
+    #[runtime::pallet_index(2)]
+    pub type Balances = pallet_balances::Pallet<Test>;
 }
 
-#[derive_impl(frame_system::config_preludes::TestDefaultConfig as frame_system::DefaultConfig)]
-impl system::Config for Test {
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
+impl frame_system::Config for Test {
 	type Block = Block;
-	type Nonce = u32;
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type RuntimeOrigin = RuntimeOrigin;
-	type RuntimeCall = RuntimeCall;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type RuntimeEvent = RuntimeEvent;
-	type BlockHashCount = BlockHashCount;
-	type Version = ();
-	type PalletInfo = PalletInfo;
 	type AccountData = pallet_balances::AccountData<Balance>;
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = SS58Prefix;
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
-	type RuntimeTask = ();
-	type SingleBlockMigrations = ();
-	type MultiBlockMigrator = ();
-	type PreInherents = ();
-	type PostInherents = ();
-	type PostTransactions = ();
-}
-
-parameter_types! {
-	pub const ExistentialDeposit: u128 = 1;
-	pub const MockLockIdentifier: [u8; 8] = *b"signal  ";
 }
 
 impl pallet_balances::Config for Test {
@@ -71,13 +44,22 @@ impl pallet_balances::Config for Test {
 	type Balance = Balance;
 	type RuntimeEvent = RuntimeEvent;
 	type DustRemoval = ();
-	type ExistentialDeposit = ExistentialDeposit;
+    type ExistentialDeposit = ConstU128<1>;
 	type AccountStore = System;
 	type WeightInfo = ();
 	type FreezeIdentifier = [u8; 8];
 	type MaxFreezes = ();
 	type RuntimeHoldReason = ();
 	type RuntimeFreezeReason = ();
+    type DoneSlashHandler = ();
+}
+
+// Define a LockId type for the signal pallet
+pub struct SignalLockId;
+impl frame_support::traits::Get<[u8; 8]> for SignalLockId {
+    fn get() -> [u8; 8] {
+        *b"signal  "
+    }
 }
 
 impl pallet_signal::Config for Test {
@@ -85,13 +67,12 @@ impl pallet_signal::Config for Test {
 	type WeightInfo = ();
 	type Currency = Balances;
 	type MaxSize = ConstU32<1024>;
-	type LockId = MockLockIdentifier;
+    type LockId = SignalLockId;
 	type LockPrice = ConstU32<10>;
 }
 
-// Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	RuntimeGenesisConfig { system: Default::default(), balances: Default::default() }
+    frame_system::GenesisConfig::<Test>::default()
 		.build_storage()
 		.unwrap()
 		.into()
